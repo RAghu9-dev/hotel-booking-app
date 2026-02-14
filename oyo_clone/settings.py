7514ce2915c52a24ab7897644a8dd31db669fd14
RAghu9-dev <singireddyraghuvardhanreddy@gmail.com>
2026-02-14 21:36:02 +0530

    Updated navbar feature

--- oyo_clone/settings.py
+++ oyo_clone/settings.py
@@ -93,6 +93,14 @@ DATABASES = {
     )
 }
 
+# If DATABASE_URL is not set, use SQLite locally
+if not DATABASES['default']:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+
+
 
 
 
